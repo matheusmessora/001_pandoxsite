e1b7165bec2fe9d50169893fbfa30a83679574d2
Matheus Messora <matheus.messora.vpn@gmail.com>
2013-01-05 11:32:03 -0200

    update database

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Site::Application.config.secret_token = 'e64b7e7cd9e6b9bcbc210b5e778be2e7fbea0266669f09e16e8f6aab2ece386a66e58b1139edbf3d58dde8f7f365c0f6d4fc64a1424ff8acf0411b9d73e742f2'
