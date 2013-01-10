83389fb698a1500b425ae20355e6c89d23173170
Matheus Messora <matheus.messora.vpn@gmail.com>
2013-01-10 00:04:25 -0200

    Remocao da deprecacao do objeto <mail>

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,18 +1,14 @@
 class ContactMailer < ActionMailer::Base
-  default	from: "from@example.com",
-  			to: "matheus.messora.vpn@gmail.com",
-			subject: "TESTE"
+  default from: "from@example.com",
+        to: "matheus.messora.vpn@gmail.com",
+      subject: "TESTE"
 
 
 
   def welcome(nome, email)
-  	logger.info "Enviando email #{nome} - #{email}"
-  	@name = nome
-	mail(:to => "matheus.messora.vpn@gmail.com",
-		:from => email) do |format|
-
-		format.text { render "welcome.txt.erb"}
-		format.html
-	end
+    logger.info "Enviando email #{nome} - #{email}"
+    @name = nome
+    mail( :to => "matheus.messora.vpn@gmail.com",
+          :from => email)
   end
 end
